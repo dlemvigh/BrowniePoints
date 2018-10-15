48b5e04a81ebcacd1d49d32dc04b0273acadfd20
David Lemvigh <dlemvigh@gmail.com>
2018-10-15 22:28:57 +0200

    Updates

---
+++ precache-manifest.8eb89544e34af12126020f74d8b452e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "81a2e97abba94dbc6699",
+    "url": "/BrowniePoints/static/js/runtime~main.81a2e97a.js"
+  },
+  {
+    "revision": "1e5ada137376070ae589",
+    "url": "/BrowniePoints/static/js/main.1e5ada13.chunk.js"
+  },
+  {
+    "revision": "0fe884be185c019a6772",
+    "url": "/BrowniePoints/static/js/1.0fe884be.chunk.js"
+  },
+  {
+    "revision": "1e5ada137376070ae589",
+    "url": "/BrowniePoints/static/css/main.0664d49f.chunk.css"
+  },
+  {
+    "revision": "3c57857c43f3662de264d403cccca1dc",
+    "url": "/BrowniePoints/index.html"
+  }
+];
